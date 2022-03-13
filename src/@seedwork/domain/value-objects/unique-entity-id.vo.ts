4354d3af052fbb6feb3330f500bb78fd7f847f29
Thiago Silva <thiago.silva@hotmail.com>
2022-03-13 21:54:32 +0000

    feat: conversao do value object para string

--- src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
+++ src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
@@ -9,7 +9,7 @@ export default class UniqueEntityId extends ValueObject<string> {
   }
 
   private validate() {
-    const isValid = uuidValidate(this.id);
+    const isValid = uuidValidate(this.value);
     if (!isValid) {
       throw new InvalidUuidError();
     }
--- src/@seedwork/domain/value-objects/value-object.ts
+++ src/@seedwork/domain/value-objects/value-object.ts
@@ -8,4 +8,19 @@ export default abstract class ValueObject<Value = any> {
   get value(): Value {
     return this._value;
   }
+
+  toString = () => {
+    if (typeof this.value !== "object" || this.value === null) {
+      try {
+        return this.value.toString();
+      } catch (error) {
+        return this.value + "";
+      }
+    }
+    const valueStr = this.value.toString();
+
+    return valueStr === "[object Object]"
+      ? JSON.stringify(this.value)
+      : valueStr;
+  };
 }
